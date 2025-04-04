755a9d749e8fcfe8deec72b03ed6ab20a3603399
ddiagilevv <134507138+ddiagilevv@users.noreply.github.com>
2025-04-05 01:15:09 +0400

    abc

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,10 +1,11 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// src/database/typeorm.config.ts
+import { DataSourceOptions } from 'typeorm';
 import { configuration } from '../config/configuration';
 import { User } from '../users/entities/user.entity';
 
 const config = configuration();
 
-export const typeormConfig: TypeOrmModuleOptions = {
+export const typeormConfig: DataSourceOptions = {
   type: 'postgres',
   host: config.database.host,
   port: config.database.port,
@@ -12,6 +13,8 @@ export const typeormConfig: TypeOrmModuleOptions = {
   password: config.database.password,
   database: config.database.name,
   entities: [User],
-  synchronize: true, // в продакшене лучше false + миграции
-  logging: false,
+  synchronize: false,           // Отключаем автоматическую синхронизацию
+  migrationsRun: true,          // Автоматически запускаем миграции при старте
+  migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
+  logging: process.env.NODE_ENV !== 'production',
 };
